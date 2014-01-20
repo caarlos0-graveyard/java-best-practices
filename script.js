c55d62df25f66bc92a4ee5115b9e312e7fc0a5b6
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-01-20 13:31:09 -0200

    rework

---
+++ script.js
@@ -0,0 +1,33 @@
+var app = angular.module('app', ['ngRoute'])
+
+app.directive("markdown", function ($compile, $http) {
+    var converter = new Showdown.converter()
+    return {
+        restrict: 'E',
+        replace: true,
+        link: function (scope, element, attrs) {
+            if ("src" in attrs) {
+                $http.get(attrs.src).then(function (data) {
+                    element.html(converter.makeHtml(data.data))
+                })
+            } else {
+                element.html(converter.makeHtml(element.text()))
+            }
+        }
+    }
+})
+
+app.controller('mainController', function ($scope, $routeParams) {
+     $scope.templateUrl =  '/pages/' + $routeParams.name + '.html'
+})
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl : 'pages/home.html'
+    })
+    .when('/:name', {
+      template: '<article ng-include="templateUrl"></article>',
+      controller: 'mainController'
+    })
+})
